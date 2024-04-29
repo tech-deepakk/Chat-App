63fd6d8f796affcf27e642a4a3e90c912934081f
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-04-29 20:58:03 +0530

    add env variable

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
-  res.json({ data: "Hello World" });
+  // this is the root
+  res.send("Hello World");
 });
-app.listen(5000, () => {
-  console.log("server started");
+
+app.listen(PORT, () => {
+  console.log(`server started at ${PORT}`);
 });
