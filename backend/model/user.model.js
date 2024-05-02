e8a9dfb1bc46e4329a1b0a2ed2cdd3f7dc40161f
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-02 22:36:58 +0530

    add model and controller of signup path and write login of sign up and generate cookie & hash password for security

---
+++ backend/model/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("userinfos", userSchema);
+export default User;
