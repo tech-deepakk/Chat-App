d3d59cd7c6178615f78cae30bf6c57ef971ebcff
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-03 19:16:28 +0530

    add login controller to the route and write logic for login

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -49,8 +49,31 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  res.send("this is a login path");
+export const login = async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+    const user = await User.findOne({ userName });
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(404).json({ error: "Invalid username or password" });
+    }
+
+    generateTokenAndCookie(user._id, res);
+
+    res.status(201).json({
+      id: user._id,
+      fullName: user.fullName,
+      userName: user.userName,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("error in login controller ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const logOut = (req, res) => {
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,13 +1,11 @@
-import express, { Router } from "express";
-import { signUp } from "../controller/auth.controller.js";
+import express from "express";
+import { login, signUp } from "../controller/auth.controller.js";
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/logout", (req, res) => {
   res.send("this is a logout path");
 });
-router.post("/login", (req, res) => {
-  res.send("this is a signup path");
-});
+router.post("/login", login);
 
 export default router;
