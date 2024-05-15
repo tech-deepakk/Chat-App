e280a6ee06877ffd5e0f9853dd2348b60568bbf7
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-15 17:11:58 +0530

    include origin and credentials to send  cookie at specific port

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -3,7 +3,9 @@ import User from "../model/user.model.js";
 
 const protectRoute = async (req, res, next) => {
   try {
+    
     const token = req.cookies.jwt;
+
     if (!token) {
       return res
         .status(401)
