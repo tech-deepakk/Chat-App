27aa47c77e7df49dc84d9bb64fb1cfacc7c4144a
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-17 14:48:38 +0530

    set the chat functionality dynamically

--- frontend/src/components/message/MessageContainer.jsx
+++ frontend/src/components/message/MessageContainer.jsx
@@ -1,18 +1,28 @@
+import { useEffect } from "react";
+import useConversation from "../zustand/useConversation";
 import MessageInput from "./MessageInput";
 import MessageList from "./MessageList";
 import { TiMessages } from "react-icons/ti";
-
 function MessageContainer() {
-  const NoChat = true;
+  const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    // cleanup function for selected user
+    return () => {
+      setSelectedConversation(null);
+    };
+  }, [setSelectedConversation]);
+
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      {NoChat ? (
+      {!selectedConversation ? (
         <NoChatSelected></NoChatSelected>
       ) : (
         <>
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="lable-text"> To : </span>
-            <span className="text-gray-900 font-bold"> Deepak Pan</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation.fullName}
+            </span>
           </div>
           <MessageList></MessageList>
           <MessageInput></MessageInput>
