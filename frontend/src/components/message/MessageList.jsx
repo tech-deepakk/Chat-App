3540e36ac89cbc487dc0e0f58b6f3fdf33b5287d
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-18 16:54:22 +0530

    create a hook that fetch the messages and return it , add a skeleton when the data is loading

--- frontend/src/components/message/MessageList.jsx
+++ frontend/src/components/message/MessageList.jsx
@@ -1,26 +1,29 @@
+import useGetMessages from "../../hooks/useGetMessages";
+import MessageSkeleton from "../skeleton/MessageSkeleton";
 import Message from "./Message";
 
 function MessageList() {
+  const { messages, loading } = useGetMessages();
+
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
-      <Message></Message>
+      {!loading &&
+        messages.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
+      {loading && (
+        <>
+          <MessageSkeleton />
+          <MessageSkeleton />
+          <MessageSkeleton />
+        </>
+      )}
+      {!loading && messages.length === 0 && (
+        <p className="text-center text-white">
+          Send a Message to start the conversation
+        </p>
+      )}
     </div>
   );
 }
---
+++ frontend/src/hooks/useGetMessages.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import useConversation from "../components/zustand/useConversation";
+
+function useGetMessages() {
+  const [loading, setLoading] = useState(false);
+  const { messages, setMessages, selectedConversation } = useConversation();
+  useEffect(() => {
+    const getMessage = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `http://localhost:5000/api/messages/${selectedConversation._id}`,
+          {
+            method: "get",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+          }
+        );
+        const data = await res.json();
+        if (data.error) {
+          throw Error(data.error);
+        }
+        setMessages(data);
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    if (selectedConversation?._id) getMessage();
+  }, [selectedConversation._id, setMessages]);
+
+  return { messages, loading };
+}
+
+export default useGetMessages;
