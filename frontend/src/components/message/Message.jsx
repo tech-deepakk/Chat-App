f002637d01e15727ef21f58396aece54a9f8ac9e
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-19 14:52:25 +0530

    make message box functional to sender and receiver

--- frontend/src/components/message/Message.jsx
+++ frontend/src/components/message/Message.jsx
@@ -1,17 +1,29 @@
-function Message() {
+import { useAuthContext } from "../context/AuthContext";
+import useConversation from "../zustand/useConversation";
+
+function Message({ message }) {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const mySide = message.senderId === authUser.id;
+  const chatSide = mySide ? "chat-end" : "chat-start";
+  const profilePic = mySide
+    ? authUser.profilePic
+    : selectedConversation.profilePic;
+  const bubbleBgColor = mySide ? "bg-blue-500" : "bg-gray-700";
+
   return (
-    <div className=" chat chat-end">
+    <div className={`chat ${chatSide}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            alt="user image"
-          />
+          <img src={profilePic} alt="user image" />
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>Hi whats up</div>
-      <div className='chat-footer opacity-50 text-xs flex gap-1 items-center'>12.50</div>
-
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
+      </div>
+      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-black">
+        12.50
+      </div>
     </div>
   );
 }
