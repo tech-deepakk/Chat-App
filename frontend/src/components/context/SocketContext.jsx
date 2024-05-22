433b5007d4854ba09e9c11b9ddd2ab80bdaeab53
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-22 15:10:48 +0530

    install socket.io-client and crete a context for socket amd provide it

---
+++ frontend/src/components/context/SocketContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
+
+export const SocketContext = createContext();
+
+export const useSocketContext = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const [onlineUser, setOnlineUser] = useState([]);
+  const { authUser } = useAuthContext();
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:5000", {
+        query: {
+          userId: authUser._id,
+        },
+      });
+      setSocket(socket);
+
+      return () => socket.close();
+    } else {
+      if (socket) {
+        socket.close();
+        setSocket(null);
+      }
+    }
+  }, []);
+
+  return (
+    <SocketContext.Provider value={{ socket, onlineUser }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
