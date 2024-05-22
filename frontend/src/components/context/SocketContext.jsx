a0794ac22fba6578e8ed3030d6c9f562cbb175fe
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-22 16:43:03 +0530

    set the functionality to show the online users

--- frontend/src/components/context/SocketContext.jsx
+++ frontend/src/components/context/SocketContext.jsx
@@ -22,6 +22,10 @@ export const SocketContextProvider = ({ children }) => {
       });
       setSocket(socket);
 
+      socket.on("getOnlineUsers", (users) => {
+        setOnlineUser(users);
+      });
+
       return () => socket.close();
     } else {
       if (socket) {
