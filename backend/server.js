aaf1576ea3604d2b3793168e5746b4a98552e5bd
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-23 15:54:04 +0530

    include the paths to run frontend from the server and write script

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/authRoute.js";
@@ -9,8 +10,9 @@ import cors from "cors";
 import { app, server } from "./socket/socket.js";
 
 dotenv.config();
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,9 +22,10 @@ app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
 
-app.get("/", (req, res) => {
-  // this is the root
-  res.send("Hello World");
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
 async function connetDb() {
