a6dc6d99b386581a6406523d7b57c7f65bffffcc
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-05-23 14:50:34 +0530

    delete useless import of app.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
