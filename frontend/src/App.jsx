1e11eda8ba706e75b12ca3a946726b193143d4c9
tech-deepakk <tech.deepakpanchal@gamil.com>
2024-07-12 13:24:22 +0530

    add the message component for small screen and add css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,32 +4,38 @@ import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
 import { Toaster } from "react-hot-toast";
 import { useAuthContext } from "./components/context/AuthContext";
+import ScreenMess from "./components/home/ScreenMess";
 
 function App() {
   const { authUser, setAuthUser } = useAuthContext();
 
   return (
-    <div className="p-4 h-screen flex items-center justify-center">
-      <Routes>
-        <Route
-          path="/"
-          element={authUser ? <Navigate to="/home" /> : <Login />}
-        />
-        <Route
-          path="/login"
-          element={authUser ? <Navigate to="/home" /> : <Login />}
-        />
-        <Route
-          path="/home"
-          element={!authUser ? <Navigate to="/login" /> : <Home />}
-        />
-        <Route
-          path="/signup"
-          element={authUser ? <Navigate to="/home" /> : <SignUp />}
-        />
-      </Routes>
-      <Toaster />
-    </div>
+    <>
+      <div className=" hidden md:flex p-4 h-screen  items-center justify-center">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route
+            path="/home"
+            element={!authUser ? <Navigate to="/login" /> : <Home />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/home" /> : <SignUp />}
+          />
+        </Routes>
+        <Toaster />
+      </div>
+      <div className="md:hidden h-screen flex justify-center items-center">
+        <ScreenMess></ScreenMess>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/home/ScreenMess.jsx
@@ -0,0 +1,7 @@
+function ScreenMess() {
+  return (
+    <div className="text-3xl font-semibold text-center  text-gray-100">Please Open the Desktop Mode</div>
+  )
+}
+
+export default ScreenMess
